2456c240f501c2aaf294e9f2aece4e97c092ce1d
quickheaven <quckheaven@gmail.com>
2020-01-01 15:39:41 -0500

    First React Application.

---
+++ react-bootcamp/portfolio/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class App extends Component {
+
+    render() {
+        return (
+        <div>
+            <h1>Hello!</h1>
+            <p>My name is Ajie. I'm a application developer</p>
+            <p>I'm always looking forward to working on a meaningful projects.</p>
+            <p></p>
+        </div>)
+    }
+
+}
+
+export default App;
