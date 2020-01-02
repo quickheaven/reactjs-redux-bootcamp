cca1ec069479484220d87da63c74f13d50b901c8
quickheaven <quckheaven@gmail.com>
2020-01-02 00:38:54 -0500

    Wrap up with Low-Hanging Styling Fruit.

--- react-bootcamp/portfolio/src/App.js
+++ react-bootcamp/portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.png';
 
 class App extends Component {
     state = { displayBio: false };
@@ -12,6 +13,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <img src={profile} alt='profile' className="profile" />
                 <h1>Hello!</h1>
                 <p>My name is Ajie. I'm a application developer</p>
                 <p>I'm always looking forward to working on a meaningful projects.</p>
