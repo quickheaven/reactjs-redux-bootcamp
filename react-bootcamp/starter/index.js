e31100131e8f36f3973acffba67555d252fb2cd1
quickheaven <quckheaven@gmail.com>
2020-01-02 16:55:33 -0500

    Compiling, Bundling, and a Closer Look at JSX.

--- react-bootcamp/starter/index.js
+++ react-bootcamp/starter/index.js
@@ -1,5 +1,7 @@
-console.log('hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const element = React.createElement('div', null, 'React Element!');
+// const element = React.createElement('div', null, 'React Element!');
+const element = <div>React element with JSX!</div>;
 
 ReactDOM.render(element, document.getElementById('root'));
