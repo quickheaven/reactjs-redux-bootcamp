e5ed1719b4bd696058f730894a23df88dae290ca
quickheaven <quckheaven@gmail.com>
2020-01-02 22:12:04 -0500

    Challenge and Code: Ten More Jokes

--- react-bootcamp/portfolio/src/components/Jokes.js
+++ react-bootcamp/portfolio/src/components/Jokes.js
@@ -1,7 +1,11 @@
 import React, { PureComponent, Component } from 'react';
 
+const Joke = ({ joke: { setup, punchline } }) => (
+    <p style={{ margin: 20 }}>{setup} <em>{punchline}</em></p>
+)
+
 class Jokes extends Component {
-    state = { joke: {} };
+    state = { joke: {}, jokes: [] };
 
     componentDidMount() {
         fetch('https://official-joke-api.appspot.com/random_joke')
@@ -9,12 +13,21 @@ class Jokes extends Component {
             .then(json => this.setState({ joke: json }));
     }
 
+    fetchJokes = () => {
+        fetch('https://official-joke-api.appspot.com/random_ten')
+            .then(response => response.json())
+            .then(json => this.setState({ jokes: json }));
+    }
+
     render() {
-        const { setup, punchline } = this.state.joke;
         return (
             <div>
                 <h2>Higlighted Joke</h2>
-                <p>{setup}</p> <em>{punchline}</em>
+                <Joke joke={this.state.joke} />
+                <hr />
+                <h3>Want ten new jokes?</h3>
+                <button onClick={this.fetchJokes}>Click me!</button>
+                {this.state.jokes.map(joke => (<Joke key={joke.id} joke={joke} />))}
             </div>
         )
     }
