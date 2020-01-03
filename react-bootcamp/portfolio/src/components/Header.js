d2e617964494855ab2b3a16720619829f7d310e6
quickheaven <quckheaven@gmail.com>
2020-01-03 11:25:44 -0500

    Higher Order Components.

--- react-bootcamp/portfolio/src/components/Header.js
+++ react-bootcamp/portfolio/src/components/Header.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-
+const Header = ({children}) => {
     const style = {
         display: 'inline-block',
         margin: 10,
         marginBottom: 30
     };
 
-    return (<div>
-        <h3 style={style}><Link to='/'>Home</Link></h3>
-        <h3 style={style}><Link to='/jokes'>Jokes</Link></h3>
-    </div>
+    return (
+        <div>
+            <div>
+                <h3 style={style}><Link to='/'>Home</Link></h3>
+                <h3 style={style}><Link to='/jokes'>Jokes</Link></h3>
+            </div>
+            {children}
+        </div>
     )
 }
 
