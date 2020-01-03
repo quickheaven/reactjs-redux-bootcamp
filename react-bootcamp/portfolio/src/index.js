d9cc43f4f47973ce35c95a3d9241d0b6560bdbf1
quickheaven <quckheaven@gmail.com>
2020-01-03 10:59:48 -0500

    React Router

--- react-bootcamp/portfolio/src/index.js
+++ react-bootcamp/portfolio/src/index.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { Router, Switch, Route } from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory';
+import App from "./components/App";
+import Jokes from "./components/Jokes";
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-new Promise((resolve, reject) => {
-    return reject(new Error('no bears'));
-    /*
-    setTimeout(() => {
-        resolve('Bears, Beets, Battlestar Galatica');
-    }, 2000);
-    */
-})
-    .then(quote => {
-        console.log(quote);
-    })
-    .catch(error => console.log('error', error
-    ))
-;
+ReactDOM.render(
+    <Router history={createBrowserHistory()}>
+        <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/jokes' component={Jokes} />
+        </Switch>
+    </Router>,
+    document.getElementById('root'));
