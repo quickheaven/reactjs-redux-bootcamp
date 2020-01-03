1184100ee506922605e9c055c363c5122296a573
quickheaven <quckheaven@gmail.com>
2020-01-03 11:11:53 -0500

    Header Component

--- react-bootcamp/portfolio/src/components/Jokes.js
+++ react-bootcamp/portfolio/src/components/Jokes.js
@@ -1,5 +1,6 @@
 import React, { PureComponent, Component } from 'react';
-
+import Header from './Header';
+5
 const Joke = ({ joke: { setup, punchline } }) => (
     <p style={{ margin: 20 }}>{setup} <em>{punchline}</em></p>
 )
@@ -24,6 +25,7 @@ class Jokes extends Component {
     render() {
         return (
             <div>
+                <Header />
                 <h2>Higlighted Joke</h2>
                 <Joke joke={this.state.joke} />
                 <hr />
