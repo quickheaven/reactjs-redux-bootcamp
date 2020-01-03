b3904d74f7b0bb16f08965e11639a77b997d718f
quickheaven <quckheaven@gmail.com>
2020-01-03 10:38:30 -0500

    Fetch Under the Hood: Promises

--- react-bootcamp/portfolio/src/index.js
+++ react-bootcamp/portfolio/src/index.js
@@ -3,4 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+new Promise((resolve, reject) => {
+    return reject(new Error('no bears'));
+    /*
+    setTimeout(() => {
+        resolve('Bears, Beets, Battlestar Galatica');
+    }, 2000);
+    */
+})
+    .then(quote => {
+        console.log(quote);
+    })
+    .catch(error => console.log('error', error
+    ))
+;
