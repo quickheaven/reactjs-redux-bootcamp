f561ba5bc40ca3df24ee7d4b78d9f37896aafa09
quickheaven <quckheaven@gmail.com>
2020-01-25 19:59:47 -0500

    Tracks Component.

--- react-bootcamp/music-master/src/components/App.js
+++ react-bootcamp/music-master/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Artist from './Artist';
+import Tracks from './Tracks';
 
 const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
 
@@ -46,6 +47,7 @@ class App extends Component {
                     placeholder='Search for an Artist' />
                 <button onClick={this.searchArtist}>Search</button>
                 <Artist artist={this.state.artist} />
+                <Tracks tracks={this.state.tracks} />
             </div>)
     }
 }
