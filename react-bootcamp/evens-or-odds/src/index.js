1be512f1358c58024ae4e785a430d5b0ff4e6c85
Arjie Cristobal <ajie.io@yahoo.com>
2022-10-08 22:52:40 -0400

    evens or odds

---
+++ react-bootcamp/evens-or-odds/src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import App from "./components/App";
+import './index.css';
+
+const DEFAULT_SETTINGS = {
+    gameStarted: false,
+    instructionsExpanded: false
+}
+
+const rootReducer = (state = DEFAULT_SETTINGS, action) => {
+
+    if (action.type === 'SET_GAME_STARTED') {
+        return {
+            gameStarted: action.gameStarted,
+            instructionsExpanded: false
+        }
+    }
+
+    return state;
+};
+
+const store = createStore(rootReducer);
+
+store.subscribe(() => console.log('store.getState()', store.getState()));
+
+const action1 = { gameStarted: true, type: 'SET_GAME_STARTED' };
+
+store.dispatch(action1);
+
+store.dispatch({type: 'foo'});
+store.dispatch({type: 'bar'});
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));
