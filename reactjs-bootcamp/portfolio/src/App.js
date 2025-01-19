8a8c041d8e87eeb468baa6a4f018a828cf499515
ajie.io <ajie.io@yahoo.com>
2025-01-18 21:09:00 -0500

    Topic #9 A React Component

---
+++ reactjs-bootcamp/portfolio/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Arjie. I'm a software developer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+            </div>
+        )
+    }
+}
+
+export default App;
