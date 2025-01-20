75f3af04b9ba31a0b2d3e829b357b4b4c9cd0601
ajie.io <ajie.io@yahoo.com>
2025-01-19 20:12:58 -0500

    Topic #20 Wrap up with Low-Hanging Styling Fruit

--- reactjs-bootcamp/portfolio/src/App.js
+++ reactjs-bootcamp/portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Projects from './Projects';
 import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.png'
 
 /*
 class RegularClass {}
@@ -53,6 +54,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <img src={profile} alt='profile' className='profile' />
                 <h1>Hello!</h1>
                 <p>My name is Arjie. I'm a software developer.</p>
                 <p>I'm always looking forward to working on meaningful projects.</p>
