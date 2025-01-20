c1fc6dc1746eff1b0e3cfd8ab7ba5887c2963851
ajie.io <ajie.io@yahoo.com>
2025-01-19 19:28:59 -0500

    Topic #17 Props and Project Component

--- reactjs-bootcamp/portfolio/src/Projects.js
+++ reactjs-bootcamp/portfolio/src/Projects.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
 import PROJECTS from './data/projects';
 
+class Project extends Component {
+    render() {
+        console.log('this.props', this.props);
+
+        // destructuring syntax
+        const { title, image, description, link } = this.props.project;
+
+        //const title = this.props.project.title;
+        //const image = this.props.project.image;
+        //const description = this.props.project.description
+        //const link = this.props.project.link;
+
+        //return (
+        //    <div>{this.props.project.title}</div>
+        //)
+
+        return (
+            <div style={{ display: 'inline-block', width: 300, margin: 10 }}>
+                <h3>{title}</h3>
+                <img src={image} alt='profile' style={{ width: 200, height: 120 }} />
+                <p>{description}</p>
+                <a href={link}>{link}</a>
+            </div>
+        )
+    }
+}
+
 class Projects extends Component {
     render() {
         return (
@@ -10,7 +37,7 @@ class Projects extends Component {
                     {
                         PROJECTS.map(PROJECT => {
                             return (
-                                <div key={PROJECT.id}>{PROJECT.title}</div>
+                                <Project key={PROJECT.id} project={PROJECT} />
                             );
                         })
                     }
