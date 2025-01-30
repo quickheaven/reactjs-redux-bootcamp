aed50e858f42ac9615f51d31ca8531c7c5944d74
ajie.io <ajie.io@yahoo.com>
2025-01-29 22:32:33 -0500

    Topic 36 Ten More Jokes

--- reactjs-bootcamp/portfolio/src/Jokes.js
+++ reactjs-bootcamp/portfolio/src/Jokes.js
@@ -1,22 +1,58 @@
 import React, { Component } from "react";
 
+//const Joke = props => {
+//    const {setup, punchline} = props.joke;
+
+//const Joke = ({ joke }) => {
+//    const { setup, punchline } = joke;
+/*
+const Joke = ({ joke: { setup, punchline } }) => {
+    return <p style={{ margin: 20 }}>{setup} <em>{punchline}</em></p>;
+}
+*/
+const Joke = ({ joke: { setup, punchline } }) => (
+    <p style={{ margin: 20 }}>{setup} <em>{punchline}</em></p>
+)
+
+
 class Jokes extends Component {
 
-    state = { joke: {} };
+    state = { joke: {}, jokes: [] };
 
     componentDidMount() {
         fetch('https://official-joke-api.appspot.com/random_joke')
             // .then(response => { return response.json() });
             .then(response => response.json())
-            .then(json => this.setState({ joke: json}))
+            .then(json => this.setState({ joke: json }))
+    }
+
+    fetchJokes = () => {
+        fetch('https://official-joke-api.appspot.com/random_ten')
+            .then(response => response.json())
+            .then(json => this.setState({ jokes: json }))
     }
 
     render() {
-        const { setup, punchline} = this.state.joke;
+        // const { setup, punchline } = this.state.joke;
         return (
             <div>
                 <h2>Highlighted Joke</h2>
-                <p>{setup} <em>{punchline}</em></p>
+                <Joke joke={this.state.joke} />
+                <hr />
+                <h3>Want ten new jokes?</h3>
+                <button onClick={this.fetchJokes}>Click me!</button>
+                {
+                    /*
+                    // v1
+                    this.state.jokes.map(joke => {
+                        const { id, setup, punchline } = joke;
+                        return <p key={id}>{setup} <em>{punchline}</em></p>
+                    })
+                    // v2    
+                    this.state.jokes.map(joke => {return <Joke joke={this.state.joke} />})                        
+                    */
+                    this.state.jokes.map(joke => (<Joke key={joke.id} joke={joke} />))
+                }
             </div>
         )
     }
