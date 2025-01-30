8f2589bf966f11a550054928179de7f152540930
ajie.io <ajie.io@yahoo.com>
2025-01-29 22:46:23 -0500

    Topic 37 Fetch Under the Hood

--- reactjs-bootcamp/portfolio/src/Jokes.js
+++ reactjs-bootcamp/portfolio/src/Jokes.js
@@ -24,12 +24,14 @@ class Jokes extends Component {
             // .then(response => { return response.json() });
             .then(response => response.json())
             .then(json => this.setState({ joke: json }))
+            .catch(error => alert(error.message));
     }
 
     fetchJokes = () => {
         fetch('https://official-joke-api.appspot.com/random_ten')
             .then(response => response.json())
             .then(json => this.setState({ jokes: json }))
+            .catch(error => alert(error.message));
     }
 
     render() {
--- reactjs-bootcamp/portfolio/src/index.js
+++ reactjs-bootcamp/portfolio/src/index.js
@@ -5,3 +5,37 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+/*
+new Promise(resolve => {
+    setTimeout(() => {
+        console.log('Bears');
+        resolve();
+    }, 2000);
+})
+.then(() => {
+    console.log('Beets');
+    console.log('Battlestar Galactica');
+});
+*/
+/*
+new Promise(resolve => {
+    setTimeout(() => {
+        resolve('Bears, Beets, Battlestar Galactica');
+    }, 2000);
+})
+.then(quote => {
+    console.log(quote);
+});
+*/
+/*
+new Promise((resolve, reject) => {
+    return reject(new Error('No bears'));
+    setTimeout(() => {
+        resolve('Bears, Beets, Battlestar Galactica');
+    }, 2000);
+})
+.then(quote => {
+    console.log(quote);
+})
+.catch(error => console.log('error', error));
+*/
