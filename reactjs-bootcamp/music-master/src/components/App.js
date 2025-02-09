73653606d4ab234a28ad5bcd46bd3a59e925fdf9
ajie.io <ajie.io@yahoo.com>
2025-02-09 13:02:25 -0500

    Topic 44 Track User Input in the State

--- reactjs-bootcamp/music-master/src/components/App.js
+++ reactjs-bootcamp/music-master/src/components/App.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+    state = { artistQuery: '' };
+
+    updateArtistQuery = event => {
+        console.log('event.target.value', event.target.value);
+        this.setState({ artistQuery: event.target.value });
+    }
+
+    handleKeyPress = event => {
+        if (event.key === 'Enter') {
+            this.searchArtist();
+        }
+
+    }
+
+    searchArtist = () => {
+        console.log('this.state', this.state);
+    }
+
     render() {
         return (
-            <div>React App</div>
+            <div>
+                <h2>Music Master</h2>
+                <input
+                    onChange={this.updateArtistQuery}
+                    onKeyPress={this.handleKeyPress}
+                    placeholder='Search for an Artist'
+                />
+                <button onClick={this.searchArtist}>Search</button>
+            </div>
         )
     }
 }
