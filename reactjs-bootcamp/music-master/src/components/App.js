822b14fda117725ad3dd16ced14a02e6a6966482
ajie.io <ajie.io@yahoo.com>
2025-02-09 13:15:30 -0500

    Topic 45 Search an Artist's Top Tracks

--- reactjs-bootcamp/music-master/src/components/App.js
+++ reactjs-bootcamp/music-master/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com';
+
 class App extends Component {
 
-    state = { artistQuery: '' };
+    state = { artistQuery: '', artist: null, tracks: [] };
 
     updateArtistQuery = event => {
-        console.log('event.target.value', event.target.value);
         this.setState({ artistQuery: event.target.value });
     }
 
@@ -17,10 +18,26 @@ class App extends Component {
     }
 
     searchArtist = () => {
-        console.log('this.state', this.state);
+        fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+            .then(response => response.json())
+            .then(json => {
+                if (json.artists.total > 0) {
+                    const artist = json.artists.items[0];
+                    this.setState({ artist });
+
+                    fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+                        .then(response => response.json())
+                        .then(json => this.setState({ tracks: json.tracks }))
+                        .catch(error => alert(error.message));
+                }
+            })
+            .catch(error => alert(error.message));;
+
     }
 
     render() {
+        console.log('this.state', this.state);
+        
         return (
             <div>
                 <h2>Music Master</h2>
