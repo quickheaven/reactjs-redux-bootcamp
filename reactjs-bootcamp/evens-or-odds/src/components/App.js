d26e9d06d76f39cdf41a36b55324737706b191bf
ajie.io <ajie.io@yahoo.com>
2025-02-13 21:51:10 -0500

    Topic 77 Track Correct Guesses

--- reactjs-bootcamp/evens-or-odds/src/components/App.js
+++ reactjs-bootcamp/evens-or-odds/src/components/App.js
@@ -7,6 +7,7 @@ import Instructions from './Instructions';
 import DrawCard from './DrawCard';
 import Card from './Card';
 import Guess from './Guess';
+import GameState from './GameState';
 
 
 class App extends Component {
@@ -35,6 +36,8 @@ class App extends Component {
                     this.props.gameStarted ? (
                         <div>
                             <h3>The game is on!</h3>
+                            <br />
+                            <GameState />
                             <br />
                             <Guess />
                             <br />
---
+++ reactjs-bootcamp/evens-or-odds/src/components/GameState.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const GameState = ({ remaining, correctGuesses }) => {
+    const guessText = correctGuesses === 1 ? 'guess' : 'guesses';
+
+    return (
+        <div>
+            <p>{remaining} cards remaining</p>
+            <p>{correctGuesses} correct guesses</p>
+        </div>
+    )
+
+}
+
+export default connect(
+    ({
+        deck: { remaining },
+        gameState: { correctGuesses }
+    }) => ({ remaining, correctGuesses })
+)(GameState)
