a938000f001809c9fa55867f854c5f19b49d6560
ajie.io <ajie.io@yahoo.com>
2025-02-14 20:55:29 -0500

    Topic 89 Redux Devtools

--- reactjs-bootcamp/reaction/src/index.js
+++ reactjs-bootcamp/reaction/src/index.js
@@ -9,7 +9,10 @@ import { newMessage } from './actions/messages';
 import './index.css';
 
 
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 console.log('store.getState()', store.getState());
 store.subscribe(() => console.log('store.getState()', store.getState()));
