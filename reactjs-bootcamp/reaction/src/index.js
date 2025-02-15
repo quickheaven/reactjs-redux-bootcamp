3b53fc1f2414837219241d892a01752fdd939552
ajie.io <ajie.io@yahoo.com>
2025-02-14 19:54:34 -0500

    Topic 84 Pubnub Exploration

--- reactjs-bootcamp/reaction/src/index.js
+++ reactjs-bootcamp/reaction/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import App from './components/App';
 import './index.css';
+import './pubsub';
 
 const store = createStore(rootReducer);
 
