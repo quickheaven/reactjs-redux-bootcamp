492ccdfb166a912a16647500b323a1e649b512ce
ajie.io <ajie.io@yahoo.com>
2025-02-16 18:03:03 -0500

    Topic 103 GET Stories and Nodemon

--- reactjs-bootcamp/news/index.js
+++ reactjs-bootcamp/news/index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const stories = require('./stories')
 
 const app = express();
 
 app.get('/ping', (req, res) => {
-    res.send('pong');
+    res.send('pong!');
+});
+
+app.get('/stories', (req, res) => {
+    return res.json(stories);
+});
+
+app.get('/stories/:title', (req, res) => {
+    const { title } = req.params;
+
+    res.json(stories.filter(story => story.title.includes(title)));
 });
 
 const PORT = 3000;
