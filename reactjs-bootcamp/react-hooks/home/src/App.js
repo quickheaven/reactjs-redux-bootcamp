e89cab678ebf3f621234b17de1a526c7fd884a54
ajie.io <ajie.io@yahoo.com>
2025-02-19 19:20:13 -0500

    Topic 118 First Hook: useState

--- reactjs-bootcamp/react-hooks/home/src/App.js
+++ reactjs-bootcamp/react-hooks/home/src/App.js
@@ -1,6 +1,36 @@
+import { useState } from "react";
+
 function App() {
+
+  const [userQuery, setUserQuery] = useState('');
+
+  const searchQuery = () => {
+    window.open(`https://www.google.com/search?q=${userQuery}`, '_blank');
+  }
+
+  const handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  }
+
+  const updateUserQuery = event => {
+    console.log('userQuery', userQuery);
+    setUserQuery(event.target.value);
+  }
+
   return (
-    <div className="App">App</div>
+    <div className="App">
+      <h1>Hello Arjie</h1>
+      <div className="form">
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Search</button>
+      </div>
+    </div>
   );
 }
 
