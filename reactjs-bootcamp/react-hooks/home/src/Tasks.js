74fdc5d4cc9250f09adf8dbf6c8ca2b2419aea11
ajie.io <ajie.io@yahoo.com>
2025-02-21 21:03:27 -0500

    Topic 127 Tasks Component Part 2

--- reactjs-bootcamp/react-hooks/home/src/Tasks.js
+++ reactjs-bootcamp/react-hooks/home/src/Tasks.js
@@ -15,20 +15,46 @@ function Tasks() {
         setTasks([...tasks, { taskText, id: uuid() }]);
     }
 
+    const completeTask = completedTask => () => {
+        setCompletedTasks([...completedTasks, completedTask]);
+        setTasks(tasks.filter(task => task.id !== completedTask.id));
+    }
+
+    const deleteTask = task => () => {
+        setCompletedTasks(completedTasks.filter(t => t.id !== task.id));
+    }
+
     console.log('tasks', tasks);
+    console.log('completedTasks', completedTasks);
 
     return (
         <div>
             <h3>Tasks</h3>
-            <div className="form">
+            <div className='form'>
                 <input value={taskText} onChange={updateTaskText} />
                 <button onClick={addTask}>Add Task</button>
             </div>
-            <div className="task-list">
+            <div className='task-list'>
                 {
                     tasks.map(task => {
                         const { id, taskText } = task;
-                        return <div key={id}>{taskText}</div>
+                        return <div key={id} onClick={completeTask(task)}>
+                            {taskText}
+                        </div>
+                    })
+                }
+            </div>
+            <div className='completed-list'>
+                {
+                    completedTasks.map(task => {
+                        const { id, taskText } = task;
+
+                        return (
+                            <div key={id}>
+                                {taskText}{' '}
+                                <span onClick={deleteTask(task)} className='delete-tasks'>x</span>
+                            </div>
+                        )
                     })
                 }
 
