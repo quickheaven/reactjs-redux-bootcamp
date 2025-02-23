fcddcc60afb59216d7317ed78dee62ed4a317340
ajie.io <ajie.io@yahoo.com>
2025-02-22 19:43:39 -0500

    Topic 132 Dynamic Delay and Increment

--- reactjs-bootcamp/react-hooks/home/src/Gallery.js
+++ reactjs-bootcamp/react-hooks/home/src/Gallery.js
@@ -1,22 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import PICTURES from './data/pictures';
 
+const SECONDS = 1000;
+const minimumDelay = 1 * SECONDS;
+const minimumIncrement = 1;
+
+
 function Gallery() {
     const [index, setIndex] = useState(0);
+    const [delay, setDelay] = useState(3 * SECONDS);
+    const [increment, setIncrement] = useState(1);
 
     useEffect(() => {
+        console.log('delay', delay, 'increment', increment);
         const interval = setInterval(() => {
             setIndex(storedIndex => {
-                return (storedIndex + 1) % PICTURES.length
+                return (storedIndex + increment) % PICTURES.length
             });
-        }, 3000);
+        }, delay);
 
         return () => {
+            console.log('remove last interval');
             clearInterval(interval);
         };
-    }, []);
+    }, [delay, increment]);
 
-    console.log('index', index);
+    const updateDelay = event => {
+        const delay = Number(event.target.value) * SECONDS;
+
+        setDelay(delay < minimumDelay ? minimumDelay : delay);
+    }
+
+    const updateIncrement = event => {
+        const increment = Number(event.target.value);
+
+        setIncrement(increment < minimumIncrement ? minimumDelay : increment);
+    }
 
     return (
         <div className='Gallery'>
@@ -24,6 +43,16 @@ function Gallery() {
                 src={PICTURES[index].image}
                 alt='gallery'
             />
+            <div className='multiform'>
+                <div>
+                    Gallery transition delay (seconds):
+                    <input type='number' onChange={updateDelay} />
+                </div>
+                <div>
+                    Gallery Increment:
+                    <input type='number' onChange={updateIncrement} />
+                </div>
+            </div>
         </div>
     )
 }
